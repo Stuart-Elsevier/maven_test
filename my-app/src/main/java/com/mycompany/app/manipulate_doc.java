63820045e72f6fed2a87f0fbc4cd56c48eb14be7
Stuart Smith <s.smith.5@elsevier.com>
2021-01-29 16:39:48 +0000

    updated

--- my-app/src/main/java/com/mycompany/app/manipulate_doc.java
+++ my-app/src/main/java/com/mycompany/app/manipulate_doc.java
@@ -6,113 +6,132 @@ import com.aspose.words.DocumentBuilder;
 import java.io.File;
 import java.nio.file.Path;
 
+import com.aspose.words.Node;
+import com.aspose.words.NodeCollection;
+import com.aspose.words.Run;
+import com.aspose.words.NodeType;
+import com.aspose.words.CompositeNode;
+import javax.swing.*;
+import javax.swing.event.TreeExpansionEvent;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.event.TreeWillExpandListener;
+import javax.swing.tree.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import java.util.Enumeration;
+
+
+
+
+
+
 public class manipulate_doc {
-
-    String filePathDoc1;
-    
-
-    /*private void run(String inputFilePath, String outputDirectoryPath) {
-
-        MSWordInspectionConfig config = new MSWordInspectionConfig();
-        MSWordInspection msWordInspection = new MSWordInspection(config);
-        msWordInspection.createInventory(inputFilePath, outputDirectoryPath);
-      }
-    */
     /*
-      public void createInventory(String filePath, String outputDirectory) {
-        String filenameModDoc = new File(filePath).getName();
-        int lastPeriodPos = filenameModDoc.lastIndexOf('.');
-        if (lastPeriodPos > 0) {
-            filenameModDoc = filenameModDoc.substring(0, lastPeriodPos);
-        }
 
-        String filepathModDoc = Path.of(outputDirectory, "bookmarked_" + filenameModDoc + ".docx").toString();
-        String filepathOutputReport = Path.of(outputDirectory, "inventory.json").toString();
-        String dirExtractedImages = Path.of(outputDirectory, "images").toString();
+    private void remove_toc(Document document) {
 
-        try {
-            Document document = new Document(filePath);
+        
+        
+        // First remove complete gallery objects
+        NodeCollection<StructuredDocumentTag> sdtCollection = document.getChildNodes(NodeType.STRUCTURED_DOCUMENT_TAG,
+            true);
 
-            // Accept changes in document before processing
-            document.acceptAllRevisions();
-            // Remove all macros
-            document.removeMacros();
-            // Remove all comments
-            NodeCollection comments = document.getChildNodes(NodeType.COMMENT, true);
-            comments.clear();
-            // Disable tracking
-            document.setTrackRevisions(false);
-            symbol2unicode(document);
-            // try to reduce number of runs by removing not-needed formatting and collapsing runs with same formatting
-            cleanup_document(document);
-            // Remove all table of contents
-            remove_toc(document);
+        sdtCollection.forEach((StructuredDocumentTag tag) -> {
+            if (tag.getSdtType() == SdtType.DOC_PART_OBJ || tag.getSdtType() == SdtType.BUILDING_BLOCK_GALLERY) {
+                if (tag.getBuildingBlockGallery().equals("Table of Contents")) {
+                    try {
+                        tag.remove();
+                    } catch (Exception e) {
+                        LOGGER.error("Error in remove_toc", e);
+                    }
+                }
+            }
+        });
+        
 
-            DocumentBuilder builder = new DocumentBuilder(document);
+        // Now remove toc outside galleries
+        NodeCollection<FieldStart> shapeCollection = document.getChildNodes(NodeType.FIELD_START, true);
 
-            profileDocument(document, builder, filepathModDoc, filepathOutputReport, dirExtractedImages);
-        } catch (Exception e) {
-            LOGGER.error("An exception occurred while inspecting MSWORD document", e);
-        }
+        shapeCollection.forEach((FieldStart field_start) -> {
+            if (field_start.getFieldType() == FieldType.FIELD_TOC) {
+                try {
+                    field_start.getField().remove();
+                } catch (Exception e) {
+                    LOGGER.error("Error in remove_toc", e);
+                }
+            }
+        });
 
     }
-    */
-    public static String getDataDir(Class c) {
-        File dir = new File(System.getProperty("user.dir"));
-        dir = new File(dir, "src");
-        dir = new File(dir, "main");
-        dir = new File(dir, "resources");
 
-        for (String s : c.getName().split("\\.")) {
-            dir = new File(dir, s);
-            if (dir.isDirectory() == false)
-                dir.mkdir();
-        }
-        System.out.println("Using data directory: " + dir.toString());
-        return dir.toString() + File.separator;
+
+*/
+    public static void recurseAllNodes(String dataDir) throws Exception {
+        // Open a document
+        Document doc = new Document(dataDir + "Node.RecurseAllNodes.doc");
+        // Invoke the recursive function that will walk the tree.
+        traverseAllNodes(doc);
     }
 
+    public static void traverseAllNodes(CompositeNode parentNode) throws Exception {
+        // This is the most efficient way to loop through immediate children of a node.
+        for (Node childNode = parentNode.getFirstChild(); childNode != null; childNode = childNode.getNextSibling()) {
+            // Do some useful work.
+            System.out.println(Node.nodeTypeToString(childNode.getNodeType()));
+
+            // Recurse into the node if it is a composite node.
+            if (childNode.isComposite())
+                traverseAllNodes((CompositeNode) childNode);
+            }
+        }
+    
     public static void main(String[] args) throws Exception {
 
-        //String licenseContent = args[0];
-        //String submissionFilePath = args[1];
-        //String outputDirectoryPath = args[2];
-    
-        // Load Aspose License
-        //AsposeUtils.setAsposeWordLicense(licenseContent);
-    
-        //new manipulate_doc().run(submissionFilePath, outputDirectoryPath);
-
-
+        // Set the licence
+        com.aspose.words.License license = new com.aspose.words.License();
+        license.setLicense("Aspose.Words.Java.lic");
 
         // Reads the document at location: /Users/smiths/development/java_code/Maven_test/src/main/resources/com/mycompany/app/manipulate_doc/
-        String dataDir = getDataDir(manipulate_doc.class);
+        String dataDir = Utils.getDataDir(manipulate_doc.class);
         Document doc = new Document(dataDir + "Revised manuscript with no changes marked.docx");
-        System.out.println("This is the dataDir" + doc);
+        //int type = doc.getNodeType();
 
+    
+        /*
+        The following code example shows how to enumerate immediate child nodes
+        of a CompositeNode using the enumerator provided by the ChildNodes collection:
+        */
 
-/*
-        //ExStart:
-        // The path to the documents directory.
-        String dataDir = Utils.getDataDir(AppendDocuments.class);
+        Paragraph paragraph = (Paragraph) doc.getChild(NodeType.PARAGRAPH, 0, true);
 
-        // Load the destination and source documents from disk.
-        Document dstDoc = new Document(dataDir + "TestFile.Destination.doc");
-        Document srcDoc = new Document(dataDir + "TestFile.Source.doc");
-        // Append the source document to the destination document while keeping the original formatting of the source document.
-        dstDoc.appendDocument(srcDoc, ImportFormatMode.KEEP_SOURCE_FORMATTING);
-        dstDoc.save(dataDir + "TestFile Out.docx");
-        //ExEnd:
-
-        System.out.println("Documents appended successfully.");
-*/
-        // Open the document.
-        
-        //DocumentBuilder builder = new DocumentBuilder(doc);
-        //builder.writeln("Hello World!, This is Stuart");
-        //doc.save(dataDir + "HelloWorld_out_.docx");
-        // System.out.println("New Word document created successfully.");
+        NodeCollection children = paragraph.getChildNodes();
+        for (Node child : (Iterable<Node>) children) {
+        // Paragraph may contain children of various types such as runs, shapes and so on.
+        if (child.getNodeType() == NodeType.RUN) {
+        // Say we found the node that we want, do something useful.
+            Run run = (Run) child;
+            System.out.println(run.getText());
+            }
+        }
+        String dataDir2 = Utils.getSharedDataDir(NodeCollection.class) + "DocumentObjectModel/";
+        recurseAllNodes(dataDir2);
+   
 
     }
     
 }
+/*
+public static void main(String[] args) throws Exception {
+    String dataDir = Utils.getSharedDataDir(ChildNodes.class) + "DocumentObjectModel/";
+    recurseAllNodes(dataDir);
+}
+*/
+
+
+
+
+     // TO DO
+        // Remove COntents Page 
+        // Process outputted doc into HTML5
